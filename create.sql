e8ed9f5556658e4c248243fe7d1f4c7b388ce7d4
2281749028@qq.com <2281749028@qq.com>
2024-03-16 17:14:37 +0800

    修改

--- create.sql
+++ create.sql
@@ -1,26 +1,34 @@
-create table Users(
+create table Users
+(
     user_id int primary key,
     user_name varchar(50) not null,
     password varchar(50) not null,
     privilege varchar(50) check ( privilege in ('admin','patience','doctor') ) -- 多类型用户用于公开线上平台和内网管理员修改
 );
-Create table UserDetails( -- 用户个人信息，如真实姓名身份证号等，同样用于真实上线的平台，需要完善
-
+Create table UserDetails
+( -- 用户个人信息，如真实姓名身份证号等，同样用于真实上线的平台，需要完善
+    user_id int primary key,
+    constraint fk_user foreign key (user_id) references Users(user_id)
 );
-Create table TreatmentRoom( -- 诊疗室信息,暂且只支持事先录入，也可以添加功能
+Create table TreatmentRoom
+( -- 诊疗室信息,暂且只支持事先录入，也可以添加功能
     room_id int primary key,
     room_name varchar(50) not null,
     room_location varchar(100) not null,
-    room_capacity int check(room_capacity >= 0) not null --诊疗室容量，也可用于判断是否空闲
+    room_capacity int check(room_capacity >= 0) not null -- 诊疗室容量，也可用于判断是否空闲
 );
-Create table Schedules( -- 排班信息
+Create table Schedules
+( -- 排班信息
     schedule_id int primary key,
     patience_id int not null,
     doctor_id int not null,
     treatroom_id int not null,
     starttime date , -- 开始时间
     endtime date,  -- 结束时间
-    schedule_status varchar(30) -- 排班状态（要添加约束为限定的几个值，如等待，正在处理，已过期/完成）
+    schedule_status varchar(30), -- 排班状态（要添加约束为限定的几个值，如等待，正在处理，已过期/完成）
+    constraint fk_patience foreign key (patience_id) references Users(user_id),
+    constraint fk_doctor foreign key (doctor_id) references Users(user_id),
+    constraint fk_treatroom foreign key (treatroom_id) references TreatmentRoom(room_id)
     /*
         预期效果：
             第x个人等叫号，当前叫第x-1个人(第x-1个人还在使用诊疗室)
